8692c108c2da7ec8be733cdea065f8c44fa3b762
nwv5Hf <nwv5Hf@cyber-dojo.org>
2024-05-17 09:31:36 +0000

    4 ran tests, predicted green, got green

--- files/closest_to_zero_test.go
+++ files/closest_to_zero_test.go
@@ -1,10 +1,21 @@
 package ctz
 
 import (
+    "fmt"
     "testing"
+
     "github.com/stretchr/testify/assert"
 )
 
+var testResults = []struct {
+    argument []int
+    expected int
+} {
+    {[]int{}, 0},
+}
+
 func TestEmptySlice(t *testing.T) {
-    assert.Equal(t, 0, closestToZero([]int{}))
+    for _, res := range testResults {
+        assert.Equal(t, res.expected, closestToZero(res.argument), fmt.Sprintf("slice: %#v", res.argument))
+    }
 }
