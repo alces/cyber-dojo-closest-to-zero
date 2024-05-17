f9d15771034fb69b7648a445ce52dd7c23688e13
nwv5Hf <nwv5Hf@cyber-dojo.org>
2024-05-17 09:54:45 +0000

    24 ran tests, predicted green, got green

--- files/closest_to_zero.go
+++ files/closest_to_zero.go
@@ -12,9 +12,7 @@ func closestToZero(nums []int) (closest int) {
     closest = nums[0]
     
     for _, v := range nums[1:] {
-        currentDistance := math.Abs(float64(v))
-        closestDistance := math.Abs(float64(closest))
-        if currentDistance < closestDistance || (currentDistance == closestDistance && v > 0 && closest < 0) {
+        if newClosest(closest, v) {
             closest = v
         }
     }
@@ -25,6 +23,7 @@ func closestToZero(nums []int) (closest int) {
 func newClosest(champion, candidate int) bool {
     currentDistance := math.Abs(float64(candidate))
     closestDistance := math.Abs(float64(champion))
+
     if currentDistance < closestDistance {
         return true
     }
